ba4de6b7b7926d917b714532572f5687e46203aa
Eric Hein <ehein6@gatech.edu>
2016-07-18 12:54:14 -0400

    Rewrote PERF and PAPI hooks to contain all static global variables in a Singleton.

--- hooks.cc
+++ hooks.cc
@@ -3,129 +3,122 @@
 #include <stdlib.h>
 #include "json.hpp"
 using json = nlohmann::json;
-
-#include <chrono>
 using namespace std::chrono;
 
-static time_point<steady_clock> t1, t2;
+Hooks& Hooks::getInstance()
+{
+    static Hooks instance;
+    return instance;
+}
 
-#if defined(ENABLE_SNIPER_HOOKS)
-    #include <hooks_base.h>
-#elif defined(ENABLE_GEM5_HOOKS)
-    #include <util/m5/m5op.h>
-#elif defined(ENABLE_PIN_HOOKS)
-    // Nothing to include here
-#elif defined(ENABLE_PERF_HOOKS)
-    #include <perf.h>
-    #include <omp.h>
-    #include <chrono>
+#if defined(ENABLE_PERF_HOOKS)
 
-    static std::vector<std::string> getPerfEventNames()
+std::vector<std::string>
+Hooks::getPerfEventNames()
+{
+    std::vector<std::string> event_names = {"", "--perf-event"};
+    if (const char* env_names = getenv("PERF_EVENT_NAMES"))
     {
-        std::vector<std::string> event_names = {"", "--perf-event"};
-        if (const char* env_names = getenv("PERF_EVENT_NAMES"))
+        char * names = new char[strlen(env_names) + 1];
+        strcpy(names, env_names);
+        char * p = strtok(names, " ");
+        while (p)
         {
-            char * names = new char[strlen(env_names) + 1];
-            strcpy(names, env_names);
-            char * p = strtok(names, " ");
-            while (p)
-            {
-                event_names.push_back(std::string(p));
-                p = strtok(NULL, " ");
-            }
-            delete[] names;
-
-        } else {
-            printf("WARNING: No perf events found in environment; set PERF_EVENT_NAMES.\n");
+            event_names.push_back(std::string(p));
+            p = strtok(NULL, " ");
         }
-        return event_names;
-    }
+        delete[] names;
 
-    static int getPerfGroupSize()
+    } else {
+        printf("WARNING: No perf events found in environment; set PERF_EVENT_NAMES.\n");
+    }
+    return event_names;
+}
+
+int
+Hooks::getPerfGroupSize()
+{
+    if (const char* env_group_size = getenv("PERF_GROUP_SIZE"))
     {
-        if (const char* env_group_size = getenv("PERF_GROUP_SIZE"))
-        {
-            return atoi(env_group_size);
-        } else {
-            printf("WARNING: Perf group size unspecified, defaulting to 4\n");
-            return 4;
-        }
+        return atoi(env_group_size);
+    } else {
+        printf("WARNING: Perf group size unspecified, defaulting to 4\n");
+        return 4;
     }
+}
 
-    static std::vector<std::string> perf_event_names = getPerfEventNames();
-    static gBenchPerf_event perf_events(perf_event_names, false);
-    static gBenchPerf_multi perf(omp_get_max_threads(), perf_events);
-    static int perf_group_size = getPerfGroupSize();
+Hooks::Hooks()
+ : perf_event_names(getPerfEventNames())
+ , perf_group_size(getPerfGroupSize())
+ , perf_events(perf_event_names, false)
+ , perf(omp_get_max_threads(), perf_events)
+{
+
+}
 
 #elif defined(ENABLE_PAPI_HOOKS)
-    #include <papi.h>
 
-    // Get the number of elements in a static array
-    #define ARRAY_LENGTH(X) (sizeof(X)/sizeof(X[0]))
+Hooks::Hooks()
+// TODO initialize these from environment or parameter
+ : papi_counters({
+    {PAPI_TOT_INS, "total_instructions"},
+    {PAPI_LD_INS, "total_loads"},
+    {PAPI_TOT_CYC, "total_cycles"},
+    {PAPI_L3_TCA, "L3_cache_accesses"},
+    {PAPI_L3_TCM, "L3_cache_misses"},
+    {PAPI_BR_CN, "total_branches"},
+    {PAPI_BR_MSP, "branch_mispredictions"},
+    })
+{
 
-    struct papi_counter
+}
+
+void
+Hooks::papi_start()
+{
+    // Check to make sure we have enough counters
+    if (papi_counters.size() > PAPI_num_counters())
     {
-        int id;
-        const char * name;
-    };
-
-    // List of counters to record
-    static papi_counter papi_counters[] =
+        printf("Error in PAPI: not enough hardware counters (need %li, have %i)\n",
+            papi_counters.size(), PAPI_num_counters());
+        exit(1);
+    }
+    // Copy event ids to an array
+    std::vector<int> papi_event_ids(papi_counters.size());
+    for (int i = 0; i < papi_counters.size(); ++i)
     {
-        {PAPI_TOT_INS, "total_instructions"},
-        {PAPI_LD_INS, "total_loads"},
-        {PAPI_TOT_CYC, "total_cycles"},
-        {PAPI_L3_TCA, "L3_cache_accesses"},
-        {PAPI_L3_TCM, "L3_cache_misses"},
-        {PAPI_BR_CN, "total_branches"},
-        {PAPI_BR_MSP, "branch_mispredictions"},
-    };
-
-    static const int papi_num_events = ARRAY_LENGTH(papi_counters);
-
-    static int papi_event_ids[ARRAY_LENGTH(papi_counters)];
-
-    // Counter values are copied to this array after counters are stopped
-    static long long papi_values[ARRAY_LENGTH(papi_counters)];
-
-    void papi_start()
+        papi_event_ids[i] = papi_counters[i].id;
+    }
+    // Start the counters
+    if (PAPI_start_counters(&papi_event_ids[0], papi_event_ids.size()) != PAPI_OK)
     {
-        // Check to make sure we have enough counters
-        if (papi_num_events > PAPI_num_counters())
-        {
-            printf("Error in PAPI: not enough hardware counters (need %i, have %i)\n",
-                papi_num_events, PAPI_num_counters());
-            exit(1);
-        }
-        // Copy event ids to an array
-        for (int i = 0; i < papi_num_events; ++i) { papi_event_ids[i] = papi_counters[i].id; }
-        // Start the counters
-        if (PAPI_start_counters(papi_event_ids, papi_num_events) != PAPI_OK)
-        {
-            printf("Error in PAPI: failed to start counters.\n");
-            //exit(1);
-        }
+        printf("Error in PAPI: failed to start counters.\n");
+        //exit(1);
+    }
+}
+
+std::string
+Hooks::papi_stop()
+{
+    // Stop the counters
+    std::vector<long long> papi_values(papi_counters.size());
+    if (PAPI_stop_counters(&papi_values[0], papi_values.size()) != PAPI_OK)
+    {
+        printf("Error in PAPI: failed to stop counters.\n");
+        exit(1);
     }
 
-    std::string papi_stop()
+    json results;
+    for (int i = 0; i < papi_values.size(); ++i)
     {
-        // Stop the counters
-        if (PAPI_stop_counters(papi_values, papi_num_events) != PAPI_OK)
-        {
-            printf("Error in PAPI: failed to stop counters.\n");
-            exit(1);
-        }
-
-        json results;
-        for (int i = 0; i < papi_num_events; ++i)
-        {
-            results[papi_counters[i].name] = papi_values[i];
-        }
-        return results.dump();
+        results[papi_counters[i].name] = papi_values[i];
     }
+    return results.dump();
+}
 #endif
 
-int __attribute__ ((noinline)) hooks_region_begin(std::string name, int64_t trial) {
+int __attribute__ ((noinline))
+Hooks::region_begin(std::string name, int64_t trial) {
 
     #if defined(ENABLE_SNIPER_HOOKS)
         parmacs_roi_begin();
@@ -147,7 +140,8 @@ int __attribute__ ((noinline)) hooks_region_begin(std::string name, int64_t tria
     return 0;
 }
 
-int __attribute__ ((noinline)) hooks_region_end(std::string name, int64_t trial) {
+int __attribute__ ((noinline))
+Hooks::region_end(std::string name, int64_t trial) {
     t2 = steady_clock::now();
     json results;
     #if defined(ENABLE_SNIPER_HOOKS)
--- hooks.h
+++ hooks.h
@@ -1,7 +1,51 @@
 #include <string>
 #include <inttypes.h>
+#include <chrono>
+#include <vector>
 
-// Marks the start of a new phase of computation
-int hooks_region_begin(std::string name, int64_t trial);
-// Marks the end of the current phase of computation
-int hooks_region_end(std::string name, int64_t trial);
+#if defined(ENABLE_SNIPER_HOOKS)
+    #include <hooks_base.h>
+#elif defined(ENABLE_GEM5_HOOKS)
+    #include <util/m5/m5op.h>
+#elif defined(ENABLE_PIN_HOOKS)
+    // Nothing to include here
+#elif defined(ENABLE_PERF_HOOKS)
+    #include <perf.h>
+    #include <omp.h>
+#elif defined(ENABLE_PAPI_HOOKS)
+    #include <papi.h>
+#endif
+
+class Hooks
+{
+public:
+    static Hooks& getInstance();
+    // Marks the start of a new phase of computation
+	int region_begin(std::string name, int64_t trial);
+	// Marks the end of the current phase of computation
+	int region_end(std::string name, int64_t trial);
+    Hooks(Hooks const&)         = delete;
+    void operator=(Hooks const&)  = delete;
+private:
+    std::chrono::time_point<std::chrono::steady_clock> t1, t2;
+#if defined(ENABLE_PERF_HOOKS)
+    Hooks();
+    std::vector<std::string> perf_event_names;
+    gBenchPerf_event perf_events;
+    gBenchPerf_multi perf;
+    int perf_group_size;
+    std::vector<std::string> getPerfEventNames();
+    int getPerfGroupSize();
+#elif defined(ENABLE_PAPI_HOOKS)
+    Hooks();
+    struct papi_counter
+	{
+	    int id;
+	    std::string name;
+	};
+	std::vector<papi_counter> papi_counters;
+    void papi_start();
+    std::string papi_stop();
+#endif
+};
+
