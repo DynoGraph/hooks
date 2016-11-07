29b2030a7b1ce4c93221ac323be1fad02fc23f48
Eric Hein <ehein6@gatech.edu>
2016-11-07 15:43:18 -0500

    Fix tabs->spaces.

--- hooks.h
+++ hooks.h
@@ -26,21 +26,21 @@ class Hooks
 public:
     static Hooks& getInstance();
     // Marks the start of a new phase of computation
-	int region_begin(std::string name);
-	// Marks the end of the current phase of computation
-	int region_end(std::string name);
-	// Marks which trial we are in
-	int64_t trial;
-	// Marks which batch we are in
-	int64_t batch;
-	// Record the traversal of an edge during an algorithm
-	void traverse_edge(int64_t n = 1);
-	Hooks(Hooks const&)         = delete;
-	void operator=(Hooks const&)  = delete;
+    int region_begin(std::string name);
+    // Marks the end of the current phase of computation
+    int region_end(std::string name);
+    // Marks which trial we are in
+    int64_t trial;
+    // Marks which batch we are in
+    int64_t batch;
+    // Record the traversal of an edge during an algorithm
+    void traverse_edge(int64_t n = 1);
+    Hooks(Hooks const&)         = delete;
+    void operator=(Hooks const&)  = delete;
 private:
     std::ofstream out;
-	std::chrono::time_point<std::chrono::steady_clock> t1, t2;
-	std::vector<int64_t> num_traversed_edges;
+    std::chrono::time_point<std::chrono::steady_clock> t1, t2;
+    std::vector<int64_t> num_traversed_edges;
 #if defined(ENABLE_PERF_HOOKS)
     std::vector<std::string> perf_event_names;
     int perf_group_size;
